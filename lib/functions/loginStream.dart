f03050a829be62e99614efec4e8745763d9be674
Yooisae <dbdlto@naver.com>
2023-01-26 19:00:42 +0900

    add login function(google-firebase)

---
+++ lib/functions/loginStream.dart
@@ -0,0 +1,50 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:provider/provider.dart';
+
+import '../pages/homePage.dart';
+import '../pages/loginPage.dart';
+import '../provider/userProvider.dart';
+
+
+
+class LoginStream {
+  handleAuthState() {
+    return StreamBuilder(
+      stream: FirebaseAuth.instance.authStateChanges(),
+      builder: (BuildContext context, snapshot) {
+        if (snapshot.hasData) {
+
+          return const HomePage();
+        } else {
+          return const LoginPage();
+        }
+      },
+    );
+  }
+
+  void signOut() async {
+    await FirebaseAuth.instance.signOut();
+  }
+
+  Future<UserCredential> signInWithGoogle() async {
+    // Trigger the authentication flow
+    final GoogleSignInAccount? googleUser = await GoogleSignIn().signIn();
+
+    // Obtain the auth details from the request
+    final GoogleSignInAuthentication? googleAuth =
+    await googleUser?.authentication;
+
+    // Create a new credential
+    final credential = GoogleAuthProvider.credential(
+      accessToken: googleAuth?.accessToken,
+      idToken: googleAuth?.idToken,
+    );
+
+    // Once signed in, return the UserCredential
+    return await FirebaseAuth.instance.signInWithCredential(credential);
+  }
+}
---
+++ lib/provider/userProvider.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/cupertino.dart';
+
+class UserProvider extends ChangeNotifier{
+
+  UserProvider(){
+    init();
+  }
+
+  void init() async{
+
+  }
+
+}
