283d68abfa08255ec51f3f0f95a0c2dd3184a4a6
isaaclkh <isaaclkh9909@gmail.com>
2023-01-27 04:09:05 +0900

    Login 페이지 간단한 디자인 틀 구현

--- lib/pages/onBoardingPage.dart
+++ lib/pages/onBoardingPage.dart
@@ -25,26 +25,34 @@ class OnBoardingPage extends StatelessWidget {
         iconSize: 30,
         pages: [
           OnBoardingModel(
-            image: Lottie.network('https://assets3.lottiefiles.com/packages/lf20_8QyKOkeQV8.json'),
-            title: "Business Chat",
+            image: Lottie.network('https://assets5.lottiefiles.com/packages/lf20_3QnWBywaTr.json'),
+            title: "설명 1",
+            titleFontSize: 40,
+            titleFontWeight: FontWeight.bold,
             body:
             "First impressions are everything in business, even in chat service. It’s important to show professionalism and courtesy from the start",
           ),
           OnBoardingModel(
-            image: Lottie.network('https://assets3.lottiefiles.com/packages/lf20_8QyKOkeQV8.json'),
-            title: "Coffee With Friends",
+            image: Lottie.network('https://assets5.lottiefiles.com/packages/lf20_awP420Zf8l.json'),
+            title: "설명 2",
+            titleFontSize: 40,
+            titleFontWeight: FontWeight.bold,
             body:
             "When your morning starts with a cup of coffee and you are used to survive the day with the same, then all your Instagram stories and snapchat streaks would stay filled up with coffee pictures",
           ),
           OnBoardingModel(
-            image: Lottie.network('https://assets3.lottiefiles.com/packages/lf20_8QyKOkeQV8.json'),
-            title: "Mobile Application",
+            image: Lottie.network('https://assets5.lottiefiles.com/packages/lf20_7x45GFUqeu.json'),
+            title: "설명 3",
+            titleFontSize: 40,
+            titleFontWeight: FontWeight.bold,
             body:
             "Mobile content marketing has also been found to enhance quick online actions and make follow-ups easier.",
           ),
           OnBoardingModel(
             image: Lottie.network('https://assets3.lottiefiles.com/packages/lf20_8QyKOkeQV8.json'),
-            title: "Content Team",
+            title: "설명 4",
+            titleFontSize: 40,
+            titleFontWeight: FontWeight.bold,
             body: "No two content marketing teams look the same.",
           ),
         ],
