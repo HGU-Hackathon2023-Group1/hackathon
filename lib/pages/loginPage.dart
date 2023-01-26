9d3086182bd0ffb576c2e033163ff1538456e497
Yooisae <dbdlto@naver.com>
2023-01-26 20:56:21 +0900

    login edit

--- lib/pages/loginPage.dart
+++ lib/pages/loginPage.dart
@@ -1,10 +1,23 @@
 import 'package:flutter/material.dart';
 
+import '../functions/loginStream.dart';
+
 class LoginPage extends StatelessWidget {
   const LoginPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Login"),
+      ),
+      body: Center(
+        child: TextButton(
+            onPressed: () {
+              LoginStream().signInWithGoogle();
+            },
+            child: const Text("Google Login")),
+      ),
+    );
   }
 }
